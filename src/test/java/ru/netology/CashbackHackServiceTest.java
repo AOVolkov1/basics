27effbf4cb1834df15d1080cf3adb447b83a67fb
AOVolkov1 <aovolkov1@gmail.com>
2025-03-18 11:44:30 +0300

    testng

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,7 +1,30 @@
 package ru.netology;
 
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
 
+    @Test
+    public void underBoundary() {
+        CashbackHackService service=new CashbackHackService();
+        int amount=900;
+        int actual=service.remain(amount);
+        assertEquals(actual, 100);
+    }
+    @Test
+    public void aboveBoundary() {
+        CashbackHackService service=new CashbackHackService();
+        int amount=1100;
+        int actual=service.remain(amount);
+        assertEquals(actual, 900);
+    }
+    @Test
+    public void equalBoundary() {
+        CashbackHackService service=new CashbackHackService();
+        int amount=1000;
+        int actual=service.remain(amount);
+        assertEquals(actual, 0);
+    }
 }
