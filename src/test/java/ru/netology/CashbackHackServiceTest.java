7d6c64452ec1db7d4efbdbdedfbc3cb01baf3fcc
AOVolkov1 <aovolkov1@gmail.com>
2025-03-18 11:36:19 +0300

    junit4

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,7 +1,30 @@
 package ru.netology;
 
-import static org.testng.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
+    @Test
+    public void underBoundary() {
+        CashbackHackService service=new CashbackHackService();
+        int amount=900;
+        int actual=service.remain(amount);
+        assertEquals(100, actual);
+    }
+    @Test
+    public void aboveBoundary() {
+        CashbackHackService service=new CashbackHackService();
+        int amount=1100;
+        int actual=service.remain(amount);
+        assertEquals(900, actual);
+    }
+    @Test
+    public void equalBoundary() {
+        CashbackHackService service=new CashbackHackService();
+        int amount=1000;
+        int actual=service.remain(amount);
+        assertEquals(0, actual);
+    }
 }
